441db9a7806caf6202b9a74dd432ced235ebca5e
sander <sanderzet@gmail.com>
2017-07-31 22:13:33 +0300

    Renew

--- src/main/java/ua/pp/sanderzet/sanderdict/MainActivity.java
+++ src/main/java/ua/pp/sanderzet/sanderdict/MainActivity.java
@@ -42,7 +42,7 @@ String query = "";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
